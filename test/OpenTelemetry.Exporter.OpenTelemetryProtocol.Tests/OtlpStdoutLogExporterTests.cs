1c368fc2145a0f3e8a1000ca0357adf489c8dbe3
Matthew Hensley <matthew.hensley@grafana.com>
2025-01-03 11:15:16 -0500

    capture json and parse content

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpStdoutLogExporterTests.cs
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpStdoutLogExporterTests.cs
@@ -3,8 +3,12 @@
 
 #if NET8_0_OR_GREATER
 
+using System.Text;
 using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
 using Xunit;
+using OtlpLogs = OpenTelemetry.Proto.Logs.V1;
 
 namespace OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests;
 
@@ -44,6 +48,41 @@ public class OtlpStdoutLogExporterTests
         var value = key.GetProperty("stringValue");
         Assert.Equal(JsonValueKind.String, value.ValueKind);
         Assert.Equal("value", value.GetString());
+    }
+
+    [Fact]
+    public void OtlpStdoutLogExporter_Export()
+    {
+        var logRecords = new List<LogRecord>();
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddOpenTelemetry(options =>
+            {
+                options.AddInMemoryExporter(logRecords);
+            });
+        });
+        var logger = loggerFactory.CreateLogger("OtlpLogExporterTests");
+        logger.LogInformation("Hello from {name} {price}.", "tomato", 2.99);
+        using var stream = new MemoryStream();
+        using (var exporter = new OtlpStdoutLogExporter(
+            stream,
+            new OpenTelemetryProtocol.Implementation.SdkLimitOptions(),
+            new OpenTelemetryProtocol.Implementation.ExperimentalOptions()))
+        {
+            exporter.Export(new Batch<LogRecord>(logRecords.ToArray(), logRecords.Count));
+        }
+
+        Assert.NotNull(stream);
+        Assert.True(stream.Length > 0);
+        var str = Encoding.UTF8.GetString(stream.ToArray());
+        var logsData = OtlpLogs.LogsData.Parser.ParseJson(str);
+        Assert.NotNull(logsData);
+
+        var resourceLogs = Assert.Single(logsData.ResourceLogs);
+        var scopeLog = Assert.Single(resourceLogs.ScopeLogs);
+        var logRecord = Assert.Single(scopeLog.LogRecords);
+        Assert.Equal("Information", logRecord.SeverityText);
+        Assert.Equal(OtlpLogs.SeverityNumber.Info, logRecord.SeverityNumber);
     }
 }
 #endif
